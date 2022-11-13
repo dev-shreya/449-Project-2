aab9753e950c03ab8c2d95a6de75c125c1800ba0
dillonago <71573711+dillonago@users.noreply.github.com>
2022-11-13 10:35:03 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# 449-Project-2
+# CPSC449Project1
+
+## Members of Team: 
+Ayush Bhardwaj, Arati Maurya, Dillon Go, Nikhil Giri
+
+## Steps to run our program:
+1. Navigate to the correct directory of the project file. 
+2. To initialize the database, we will run it by running these lines of code:
+```
+./bin/init.sh
+```
+3. To start the service, run this line of code:
+```
+foreman start
+```
+4. After starting the service, test using HTTPie, curl, or this link:
+```
+wordle.local.gd:5000/docs
+```
---
+++ share/game.sql
@@ -0,0 +1,26 @@
+-- $ sqlite3 ./var/game.db < ./share/game.sql
+
+
+PRAGMA foreign_keys=ON;
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS Game;
+CREATE TABLE Game(game_id INTEGER PRIMARY KEY, secretword VARCHAR, guess_num INTEGER);
+
+DROP TABLE IF EXISTS In_Progress;
+CREATE TABLE In_Progress(game_id INTEGER,FOREIGN KEY(game_id) REFERENCES Game(game_id));
+
+DROP TABLE IF EXISTS Completed;
+CREATE TABLE Completed(game_id INTEGER, guess_num INTEGER, outcome VARCHAR, FOREIGN KEY(game_id) REFERENCES Game(game_id)); 
+
+DROP TABLE IF EXISTS Guesses;
+CREATE TABLE Guesses(game_id INTEGER, guess_num INTEGER, guess_word VARCHAR, FOREIGN KEY(game_id) REFERENCES Game(game_id));
+
+DROP TABLE IF EXISTS Correct_Words;
+CREATE TABLE Correct_Words(correct_word_id INTEGER PRIMARY KEY, correct_word VARCHAR);
+
+DROP TABLE IF EXISTS Valid_Words;
+CREATE TABLE Valid_Words(valid_word_id INTEGER PRIMARY KEY, valid_word VARCHAR);
+
+COMMIT;
+
